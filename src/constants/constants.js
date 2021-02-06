a65452685ad08aa82d38ce108fcab245bd451489
root <root@mail.sheetcheat.xyz>
2021-02-06 20:32:57 +0000

    j

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -11,7 +11,7 @@ const gameConfig = {
     number_of_total_turns: 24,
     mister_x_is_visible_turns: [3, 8, 13, 18, 23]
 }
-const webSocketUrl = "http://api.unseen.papero.tk";
+const webSocketUrl = "https://papero.tk/unseen";
 const flaskServerUrl = "";
 const myGames = [
     {
